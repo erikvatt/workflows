ffb7bca143fd8908e238a6290d32298f7080f645
Erik Vattekar <erik.vattekar@nav.no>
2023-10-05 14:05:57 +0200

    add delay

--- workflows/workflow1/script.py
+++ workflows/workflow1/script.py
@@ -1,8 +1,11 @@
 from google.cloud.storage import Client
 import json
+import time
 print('hello')
 
 with open("data/myfile.json", "r") as f:
     data = json.loads(f.read())
 
 print("data: ", data)
+
+time.sleep(1000)
